819eae3ef6398a6f42c79ae716c8fbe879bf60c3
maggie-j-liu <maggie.j.liu@gmail.com>
2021-08-14 09:17:44 -0700

    add about me section

--- pages/[pagename].jsx
+++ pages/[pagename].jsx
@@ -7,12 +7,12 @@ const Page = ({ name, creator, creatorName, profileImage, error }) => {
     return <div>404</div>;
   }
   return (
-    <div className={"flex flex-col items-center text-center"}>
+    <div className={"mt-8 flex flex-col w-full max-w-4xl mx-auto"}>
       {user && creator === user.uid && (
         <Link href={`/${name}/edit`}>
           <a
             className={
-              "bg-primary-300 px-2 py-1 rounded-md hover:scale-110 duration-150 my-4"
+              "fixed bottom-4 right-9 bg-primary-300 px-2 py-1 rounded-md hover:scale-110 duration-150 my-4"
             }
           >
             Edit your page
@@ -22,10 +22,16 @@ const Page = ({ name, creator, creatorName, profileImage, error }) => {
       <img
         src={profileImage}
         alt={`${creatorName}'s image`}
-        className={"h-48 w-48 rounded-full border-primary-200 border-4"}
+        className={
+          "self-center h-48 w-48 rounded-full border-primary-200 border-4"
+        }
       />
-      <h1 className={"text-3xl mt-4 font-bold"}>{creatorName}</h1>
-      <h2 className={"text-xl mt-2 text-gray-700"}>@{name}</h2>
+      <h1 className={"self-center text-3xl mt-4 font-bold"}>{creatorName}</h1>
+      <h2 className={"self-center text-xl mt-2 text-gray-700"}>@{name}</h2>
+      <div className={"mt-8"}>
+        <h3 className={"text-xl font-medium wavy"}>About Me</h3>
+        <p className={"mt-2 text-lg text-gray-500"}>Hi! I'm {creatorName}.</p>
+      </div>
     </div>
   );
 };
