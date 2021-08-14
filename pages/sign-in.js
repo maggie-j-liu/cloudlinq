474305f033e2ba469b5fd7ff6c2f80387cff1cdf
maggie-j-liu <maggie.j.liu@gmail.com>
2021-08-13 20:18:51 -0700

    poppins

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,4 +1,5 @@
 import useUser from "@/utils/useUser";
+import { FcGoogle } from "react-icons/fc";
 
 const SignIn = () => {
   const { user, signInWithGoogle } = useUser();
@@ -6,16 +7,22 @@ const SignIn = () => {
     return null;
   }
   return (
-    <div
-      className={
-        "w-full min-h-screen flex justify-center items-start bg-gray-50"
-      }
-    >
-      <div className={"bg-white px-16 py-12 mt-14"}>
+    <div className={"w-full flex justify-center items-start"}>
+      <div
+        className={
+          "bg-gray-50 px-16 py-12 mt-14 flex flex-col items-center gap-6"
+        }
+      >
+        <h1 className={"text-3xl glow font-semibold"}>
+          Welcome to <span className={"wavy"}>Cloudlinq</span>!
+        </h1>
         <button
-          className={"border-2 border-red-300 px-4 py-2 rounded-md shadow"}
+          className={
+            "bg-white glow flex items-center gap-2 justify-center border-2 border-red-300 px-4 py-2 rounded-md shadow-md hover:shadow-lg"
+          }
           onClick={() => signInWithGoogle()}
         >
+          <FcGoogle className={"h-6 w-6"} />
           Sign in with Google
         </button>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
