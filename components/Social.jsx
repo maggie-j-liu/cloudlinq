6552d67bff63f68d4c661876ab5a8e9a71d338c2
maggie-j-liu <maggie.j.liu@gmail.com>
2021-08-15 12:51:34 -0700

    placeholder text

--- components/Social.jsx
+++ components/Social.jsx
@@ -76,6 +76,7 @@ const Social = ({
             onChange={onLinkChange}
             onBlur={onLinkBlur}
             className={"w-full borderless-input"}
+            placeholder="Link"
           />
         </div>
         <div className={"w-full flex"}>
